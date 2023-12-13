eb2f611dc333b30db554ecb33815018a1032de04
sfj197 <83702958+sfj197@users.noreply.github.com>
2023-12-13 21:12:29 +0100

    Serializar.java

--- src/n1ejercicio5/Serializar.java
+++ src/n1ejercicio5/Serializar.java
@@ -5,42 +5,40 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-
 public class Serializar {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		// TODO Auto-generated method stub
-		
-		Alumno [] alumnos = new Alumno[3];
-		
-		alumnos[0] = new Alumno("Jorge",48);
-		alumnos[1] = new Alumno("Andres",34);
+
+		Alumno[] alumnos = new Alumno[3];
+
+		alumnos[0] = new Alumno("Jorge", 48);
+		alumnos[1] = new Alumno("Andres", 34);
 		alumnos[2] = new Alumno("Sandra", 40);
-		
-		
+
 		try {
-			
-			ObjectOutputStream nuevoFichero= new ObjectOutputStream(new FileOutputStream("C:/Users/Jorge/Desktop/ser/alumno.ser"));
+
+			ObjectOutputStream nuevoFichero = new ObjectOutputStream(new FileOutputStream("archivo.ser"));
 			nuevoFichero.writeObject(alumnos);
 			nuevoFichero.close();
-			
-			//Deserializamos el objeto y mostramos por consola.
-			ObjectInputStream recuperarFichero = new ObjectInputStream(new FileInputStream("C:/Users/Jorge/Desktop/ser/alumno.ser"));
-					
-				//Casting para recuperar el tipo.
-				Alumno [] deserizacion = (Alumno[])recuperarFichero.readObject();
-				
-				recuperarFichero.close();
-				
-				for(Alumno a : deserizacion) {
-					
-					System.out.println(a);
-				}
-					
-		}catch(Exception e) {
-			
+
+			// Deserializamos el objeto y mostramos por consola.
+			ObjectInputStream recuperarFichero = new ObjectInputStream(new FileInputStream("archivo.ser"));
+
+			// Casting para recuperar el tipo.
+			Alumno[] deserizacion = (Alumno[]) recuperarFichero.readObject();
+
+			recuperarFichero.close();
+
+			for (Alumno a : deserizacion) {
+
+				System.out.println(a);
+			}
+
+		} catch (Exception e) {
+
 			System.out.println("Error al recuperar fichero.");
 		}
 
-}
+	}
 }
