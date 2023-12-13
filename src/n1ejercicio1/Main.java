95a294421441c57323cacecba5f4d47fc8e0ab3a
sfj197 <83702958+sfj197@users.noreply.github.com>
2023-12-13 21:06:14 +0100

    Update Main.java

--- src/n1ejercicio1/Main.java
+++ src/n1ejercicio1/Main.java
@@ -5,12 +5,39 @@ import java.io.File;
 public class Main {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String path = "";
+		File directorio;
+		
+		if(args.length > 0) {
+			
+			path = args[0];
+		}else {
+			path = ".";
+		}
+		
+		try {
+			
+			directorio = new File(path);
+			
+			if(directorio.isDirectory() && directorio.exists()) {
+				
+				String []nombres = directorio.list();
+				
+				for(String nombre:nombres) {
+					
+					System.out.println(nombre);
+				}
+			}else {
+				System.out.println("Directorio no valido");
+			}
+			
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
 
-		File directorio = new File("C:/Users/Jorge/eclipse-workspace/Directorio/");
-		
-		LeerDirectorio nuevo = new LeerDirectorio();
-		
-		nuevo.leerDirectorio(directorio);
 	}
-	
+
 }
