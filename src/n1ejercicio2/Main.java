39f4686e6183e89dc3ba90e8e2d717bbebd8b6cf
sfj197 <83702958+sfj197@users.noreply.github.com>
2023-12-13 21:08:02 +0100

    Update Main.java

--- src/n1ejercicio2/Main.java
+++ src/n1ejercicio2/Main.java
@@ -1,20 +1,71 @@
+
 package n1ejercicio2;
 
 import java.io.File;
+import java.sql.Date;
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String path;
+		File directorio;
+
+		if (args.length > 0) {
+
+			path = args[0];
+		} else {
+			path = ".";
+		}
+
 		try {
-			File directorio = new File("C:/Users/Jorge/eclipse-workspace/Directorio");
-			LeerArbol.leer(directorio);
+			directorio = new File(path);
+			leer(directorio);
 
 		} catch (Exception e) {
 			// TODO: handle exception
 
 			System.out.println("Lectura erronea");
+		}
+
+	}
+	
+	public boolean existe(File directorio) {
+
+		if (directorio.exists() && directorio.isDirectory()) {
+
+			return true;
+		} else {
+
+			return false;
+		}
+
+	}
+
+	public static Date ultimaFecha(File directorio) {
+
+		Date ultima = new Date(directorio.lastModified());
+
+		return ultima;
+
+	}
+
+	public static void leer(File directorio) {
+
+		File[] nombresArbol = directorio.listFiles();
+
+		Arrays.sort(nombresArbol);
+
+		for (File nombre : nombresArbol) {
+
+			String esDirectorio = (nombre.isDirectory()) ? "D" : "F";
+
+			System.out.println(nombre.getName()+ " " + esDirectorio + " " + ultimaFecha(directorio));
+
+			if (nombre.isDirectory())
+				leer(nombre); 
 		}
 
 	}
